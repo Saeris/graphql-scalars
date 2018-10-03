8938df755f8aed09c527b23ca63c77c31008bd17
Drake Costa <drake@saeris.io>
2018-10-03 10:48:56 -0700

    ✨Feat Add Default Query
    Updated example app to come pre-configured with a default query for demonstration purposes.

--- example/index.js
+++ example/index.js
@@ -3,21 +3,32 @@ import { makeExecutableSchema } from "graphql-tools"
 import CustomScalars from "@saeris/graphql-scalars"
 import { types } from "./types"
 import { resolvers } from "./resolvers"
+import { defaultQuery } from "./defaultQuery"
+
+const endpoint = {
+  host: `localhost`,
+  port: 4000
+}
 
 const server = new ApolloServer({
   schema: makeExecutableSchema({
-    typeDefs: [
-      ...types,
-      ...CustomScalars.keys()
-    ],
+    typeDefs: [...types, ...CustomScalars.keys()],
     resolvers: {
       ...resolvers,
       ...CustomScalars.values()
     }
-  })
+  }),
+  playground: {
+    tabs: [
+      {
+        endpoint: `${endpoint.host}:${endpoint.port}`,
+        query: defaultQuery
+      }
+    ]
+  }
 })
 
 /* eslint-disable */
-server.listen().then(({ url }) => {
+server.listen(endpoint).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
 })
